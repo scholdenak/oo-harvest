4131047b249546bb05c76ff8e67c7c37c84d1596
vagrant <vagrant@vagrant.vm>
2020-10-09 21:39:41 +0000

    finished part three with partner

--- melons.py
+++ melons.py
@@ -1,66 +1,3 @@
-# """Classes for melon orders."""
-
-
-# class DomesticMelonOrder():
-#     """A melon order within the USA."""
-
-#     def __init__(self, species, qty):
-#         """Initialize melon order attributes."""
-
-#         self.species = species
-#         self.qty = qty
-#         self.shipped = False
-#         self.order_type = "domestic"
-#         self.tax = 0.08
-
-#     def get_total(self):
-#         """Calculate price, including tax."""
-
-#         base_price = 5
-#         total = (1 + self.tax) * self.qty * base_price
-
-#         return total
-
-#     def mark_shipped(self):
-#         """Record the fact than an order has been shipped."""
-
-#         self.shipped = True
-
-
-# class InternationalMelonOrder():
-#     """An international (non-US) melon order."""
-
-#     def __init__(self, species, qty, country_code):
-#         """Initialize melon order attributes."""
-
-#         self.species = species
-#         self.qty = qty
-#         self.country_code = country_code
-#         self.shipped = False
-#         self.order_type = "international"
-#         self.tax = 0.17
-
-#     def get_total(self):
-#         """Calculate price, including tax."""
-
-#         base_price = 5
-#         total = (1 + self.tax) * self.qty * base_price
-
-#         return total
-
-#     def mark_shipped(self):
-#         """Record the fact than an order has been shipped."""
-
-#         self.shipped = True
-
-#     def get_country_code(self):
-#         """Return the country code."""
-
-#         return self.country_code
-
-
-# _____________________________________________
-
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
     def __init__(self, species, qty, country_code = "USA"):
@@ -98,3 +35,11 @@ class InternationalMelonOrder(AbstractMelonOrder):
         else:
             total = super().get_total()
         return total
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """For government melon orders."""
+    tax = 0
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
